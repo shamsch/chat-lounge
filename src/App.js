40abf4eec28614cbdbf4d8957bddaf06039dc7da
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-26 01:04:26 +0200

    auth login and log out works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+import ChatSpace from "./components/ChatSpace";
+
+import { auth } from "./firebase/config";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 function App() {
+  const [user]= useAuthState(auth)
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <header>
+        <SignOut/>
+      </header>
+      <section>
+        {user? <ChatSpace/> : <SignIn/>}
+      </section>
+    </>
   )
 }
 
